2ee5ab27f2cd6b4770bc2f4527e58edf41aaa674
Dimon <fearshots@gmail.com>
2017-10-22 23:03:40 +0300

    init

---
+++ controllers/CategoryController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimon
+ * Date: 15.10.2017
+ * Time: 18:00
+ */
+
+namespace app\controllers;
+
+use app\models\Category;
+use app\models\Product;
+use  Yii;
+use yii\data\Pagination;
+use yii\web\HttpException;
+
+
+class CategoryController extends AppController{
+
+    public function actionIndex(){
+        $hits = Product::find()->where(['hit' => '1'])-> limit(6)->all();
+        $this->setMeta('Магазин');
+        return $this->render('index', compact('hits'));
+    }
+
+
+    public function actionView(){
+        $id = Yii::$app->request->get('id');
+
+        $category = Category::findOne($id);
+        if(empty($category))
+            throw new HttpException(404, 'Такой категории нет');
+
+        //$products = Product::find()->where(['category_id' => $id])->all();
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 3, 'forcePageParam'=> false, 'pageSizeParam'=> false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+
+        $this->setMeta('Магазин | '. $category->name, $category->keywords, $category->description);
+        return $this->render('view', compact('products', 'category', 'pages'));
+
+    }
+
+    public function actionSearch(){
+        $q = Yii::$app->request->get('q');
+        $query = Product::find()->where(['like', 'name', 'q']);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 3, 'forcePageParam'=> false, 'pageSizeParam'=> false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+        return $this->render('search', compact('products', 'q', 'pages'));
+
+    }
+
+}
